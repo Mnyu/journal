8eb770a17d73ca5d0dab812585e472df8be9afe6
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2026-06-13 03:23:01 +0530

    analytics : kpi edits, edge trend and trajectory

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,13 +1,23 @@
 import AnalyticsHeader from '@/components/analytics/analytics-header';
 import AnalyticsKPIs from '@/components/analytics/analytics-kpis';
 import BellCurveLast3Years from '@/components/analytics/analytics-bell-last-3-years';
+import EdgeTrend from '@/components/analytics/analytics-edge-trend';
+import AnalyticsTrajectoryTable from '@/components/analytics/analytics-tragetory-table';
+import AnalyticsWinRate from '@/components/analytics/analytics-win-rate';
 
 const Analytics = () => {
   return (
-    <section className='w-full flex flex-col gap-5 overflow-auto'>
+    <section className='w-full flex flex-col gap-3 p-1 overflow-auto'>
       <AnalyticsHeader />
       <AnalyticsKPIs />
-      <BellCurveLast3Years />
+      <section className='grid grid-cols-1 lg:grid-cols-2 gap-4 p-1'>
+        <EdgeTrend />
+        <BellCurveLast3Years />
+      </section>
+      <section className='grid grid-cols-1 lg:grid-cols-2 gap-4 p-1'>
+        <AnalyticsWinRate />
+        <AnalyticsTrajectoryTable />
+      </section>
     </section>
   );
 };
