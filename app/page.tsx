7e78cb23a6e89fe3e7c8389af93ba8154e8a9f5a
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2026-06-12 14:05:00 +0530

    theme toggle - dark and light

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import ThemeToggle from '@/components/theme/theme-toggle';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <>
+      <ThemeToggle />
       <h1>Hello</h1>
       <Button>Click me</Button>
     </>
